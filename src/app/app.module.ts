85438bb4874d451bb196a68ca7e56b5e575db0d0
netandout <j.diazi@uniandes.edu.co>
2021-10-15 07:19:27 -0500

    Review y Comment pre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ReviewModule} from './review/review.module';
+import {CommentModule} from './comment/comment.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReviewModule,
+        CommentModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,15 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
-};
+const host = '157.253.238.75';
+const port = '8084';
+const appName = 'frontstepbystep-api';
+const rootApi = 'api';
 
+export const environment = {
+  production: false,
+  baseUrl: `http://${host}:${port}/${appName}/${rootApi}/`
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
