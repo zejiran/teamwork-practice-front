6e5a1d7eb00d52bc20dd218ae952770ddd817770
TrashCam9 <mc.gomezh1@uniandes.edu.co>
2021-10-16 15:01:10 -0500

    Listar Outing terminada

--- src/app/outing/outing.module.ts
+++ src/app/outing/outing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { OutingListarComponent } from './outing-listar/outing-listar.component';
+
 
 @NgModule({
   imports: [
