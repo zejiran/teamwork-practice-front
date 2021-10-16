076d8ca808a30c32883b24741596bfe9ee672102
netandout <j.diazi@uniandes.edu.co>
2021-10-16 05:07:48 -0500

    correcion prueba ,HttpModule y styles Bootstrap

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('front');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('front app is running!');
-  });
 });
