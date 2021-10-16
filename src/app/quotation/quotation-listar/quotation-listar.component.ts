b23781149a606367d249367733a44fa23f362cf0
TrashCam9 <mc.gomezh1@uniandes.edu.co>
2021-10-16 16:47:20 -0500

    Invocar servicio de Quotation en componente

--- src/app/quotation/quotation-listar/quotation-listar.component.ts
+++ src/app/quotation/quotation-listar/quotation-listar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Quotation } from '../quotation';
+import { QuotationService } from '../quotation.service';
 
 @Component({
   selector: 'app-quotation-listar',
@@ -8,9 +9,19 @@ import { Quotation } from '../quotation';
 })
 export class QuotationListarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private quotationService: QuotationService) { }
+
   quotations: Array<Quotation>;
+
+  getQuotations(): void {
+    this.quotationService.getQuotations()
+      .subscribe(quotations => {
+        this.quotations = quotations;
+      });
+  }
+
   ngOnInit() {
+    this.getQuotations();
   }
 
 }
