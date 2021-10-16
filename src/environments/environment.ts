0082fcd4417084e6579e31fe5c8bf41fa7a68d78
netandout <j.diazi@uniandes.edu.co>
2021-10-15 20:26:15 -0500

    Faker y Enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,12 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+const host = 'localhost';
+const port = '8080';
+const appName = 'api';
 export const environment = {
-  production: false
+  production: false,
+  baseUrl: `http://${host}:${port}/${appName}/`
 };
 
 /*
