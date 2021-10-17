67edb8be259250b89387c30afaee9a2a6248d925
Juan Alegría <juanszalegria@gmail.com>
2021-10-17 11:27:43 -0500

    Feat: add Multimedia class

--- src/app/multimedia/multimedia.ts
+++ src/app/multimedia/multimedia.ts
@@ -1,2 +1,27 @@
+// import { Route } from '../route/route';
+// import { Post } from '../post/post';
+// import { ComplementaryInformation } from '../complementaryInformation/complementaryInformation';
+
+enum FileType {
+  VIDEO, IMAGE, AUDIO
+}
+
 export class Multimedia {
+  fileName: string;
+  mediaType: FileType;
+  extension: string;
+  // TODO: connect with missing modules
+  // route: Route;
+  // post: Post;
+  // complementaryInformation: complementaryInformation;
+
+  constructor(
+    filename: string,
+    mediaType: FileType,
+    extension: string
+  ) {
+    this.fileName = filename;
+    this.mediaType = mediaType;
+    this.extension = extension;
+  }
 }
