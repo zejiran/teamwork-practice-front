e84bb9b30b0623bc7c7d6499b59ab1a91916cdc2
Juan Alegría <juanszalegria@gmail.com>
2021-10-17 11:49:55 -0500

    Feat: complete multimedia module

--- src/app/multimedia/multimedia.module.ts
+++ src/app/multimedia/multimedia.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MultimediaListarComponent } from './multimedia-listar/multimedia-listar.component';
 
 @NgModule({
-  declarations: [MultimediaListarComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  declarations: [MultimediaListarComponent],
+  exports: [MultimediaListarComponent]
 })
 export class MultimediaModule { }
