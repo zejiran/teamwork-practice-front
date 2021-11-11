7236be0a2aba2530132e02dfb29299c7a9aab188
Juan Camach <68751794+BalaclavaAM@users.noreply.github.com>
2021-11-10 23:55:17 -0500

    hotFIX: deleted test autogenerated

--- src/app/outing/outing-detail/outing-detail.component.spec.ts
+++ src/app/outing/outing-detail/outing-detail.component.spec.ts
@@ -22,7 +22,4 @@ describe('OutingDetailComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
--- src/app/quotation/quotation-detail/quotation-detail.component.spec.ts
+++ src/app/quotation/quotation-detail/quotation-detail.component.spec.ts
@@ -18,8 +18,4 @@ describe('QuotationDetailComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
