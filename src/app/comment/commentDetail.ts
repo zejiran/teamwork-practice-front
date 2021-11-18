f1078a8dfb190b74d66a59b49390973405c9ce99
netandout <netiel136@outlook.com>
2021-11-17 23:11:10 -0500

    detail completo admin, blog, review, comment
    Falta agregar lo de la nueva clase filesContainer

--- src/app/comment/commentDetail.ts
+++ src/app/comment/commentDetail.ts
@@ -1,16 +1,14 @@
 import {Comment} from './comment';
 
 export class CommentDetail extends Comment{
-  username: string;
   userType: string;
   userId: string;
   date: Date;
   replies: Array<Comment>;
 
-  constructor(username: string, userType: string,
+  constructor(userType: string,
               userId: string, date: Date, replies: Array<Comment>) {
     super();
-    this.username = username;
     this.userType = userType;
     this.userId = userId;
     this.date = date;
