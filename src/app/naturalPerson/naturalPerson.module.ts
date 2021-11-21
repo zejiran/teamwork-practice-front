f2ca19095302c4bb62b5ab550b68151e55143e4d
Juan Alegría <j.alegria@uniandes.edu.co>
2021-11-21 14:56:47 -0500

    Feat: add natural person routing

--- src/app/naturalPerson/naturalPerson.module.ts
+++ src/app/naturalPerson/naturalPerson.module.ts
@@ -2,10 +2,12 @@ import { NaturalPersonDetailComponent } from './natural-person-detail/natural-pe
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NaturalPersonListarComponent } from './natural-person-listar/natural-person-listar.component';
+import { NaturalPersonRoutingModule } from './naturalPerson-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NaturalPersonRoutingModule
   ],
   declarations: [NaturalPersonListarComponent, NaturalPersonDetailComponent],
   exports: [NaturalPersonListarComponent, NaturalPersonDetailComponent]
