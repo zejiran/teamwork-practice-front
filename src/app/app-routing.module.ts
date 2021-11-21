13adbd05def3ee134678175216101a65f3184241
Juan Alegría <j.alegria@uniandes.edu.co>
2021-11-21 15:11:23 -0500

    Feat: add routing for home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '',   redirectTo: '/', pathMatch: 'full' }
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
