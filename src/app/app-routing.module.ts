7255afcbbf59630a9155b916dd17578af092b79c
TrashCam9 <mc.gomezh1@uniandes.edu.co>
2021-12-02 21:00:49 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { OutingListarComponent } from './outing/outing-listar/outing-listar.component';
+import { OutingCreateComponent } from './outing/outing-create/outing-create.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent},
+  { path: 'outings',
+    children: [
+      {
+        path: 'list',
+        component: OutingListarComponent
+      },
+      {
+        path: 'create',
+        component: OutingCreateComponent
+      }
+    ]}
 ];
 
 @NgModule({
